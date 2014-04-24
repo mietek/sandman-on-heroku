feb9f005555251835f47547250d6cf367735104e
Miëtek Bak <mietek@bak.io>
2014-04-24 18:25:21 +0100

    Bump

--- sandman-on-heroku.py
+++ sandman-on-heroku.py
@@ -1,3 +1,4 @@
+from flask_sslify import SSLify
 from os import getenv
 from sandman import app, auth
 from sandman.model import activate
@@ -14,6 +15,7 @@ def get_password(username):
 def before_request():
     pass
 
+sslify = SSLify(app, permanent=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
 app.debug = True
 app.wsgi_app = ProxyFix(app.wsgi_app)
